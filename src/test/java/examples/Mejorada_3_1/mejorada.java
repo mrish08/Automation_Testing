172655f16f05f5d8b574d38f3f5f953acebeee87
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-08-05 05:49:04 -1100

    Mejorada Final Activity

---
+++ src/test/java/examples/Mejorada_3_1/mejorada.java
@@ -0,0 +1,35 @@
+package examples.Mejorada_3_1;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class mejorada {
+    
+    @Test
+    public void testParallel() {
+
+        Results results = Runner.path("src/test/java/examples/Mejorada_3_1").tags("~@ignore").parallel(1);
+        //Results results = Runner.parallel(getClass(), 5);
+        generateReport(results.getReportDir());
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "Automation_Testing");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
+
+}
